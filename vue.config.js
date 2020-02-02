e626709c347d0181210e4de13bc2d5502fe2c214
Naga <nagachiangdev@gmail.com>
2020-02-02 18:01:50 +0800

    Try setting baseUrl

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/vue-tic-tac-toe/' : '/',
+  baseUrl : process.env.NODE_ENV === 'production' ? '/vue-tic-tac-toe/' : '/',
 }
