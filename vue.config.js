aaea0536ab914cfc2db4468e064ab8b2ab230a74
Naga <nagachiangdev@gmail.com>
2020-02-02 18:04:40 +0800

    Set back publicPath

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  baseUrl : process.env.NODE_ENV === 'production' ? '/vue-tic-tac-toe/' : '/',
+  publicPath : process.env.NODE_ENV === 'production' ? '/vue-tic-tac-toe/' : '/',
 }
